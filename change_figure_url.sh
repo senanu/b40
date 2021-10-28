2211c456e109494d56a945e9e9a72c7f48aac638
senanupearson <82193853+senanupearson@users.noreply.github.com>
2021-10-28 01:36:37 -0700

    Added fix for missing figures in R

--- change_figure_url.sh
+++ change_figure_url.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in docs/labs/R/*.Rmd
+for f in docs/labs/R/*.md
   do
     sed -i 's@https://github.com/senanu/b40/blob/master/b40figure/@https://senanu.github.com/b40/@'
   done
