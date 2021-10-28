083e7c9751194923de41a98fa21fd8f7f9962a69
senanupearson <82193853+senanupearson@users.noreply.github.com>
2021-10-28 07:40:34 -0700

    Still trying to get figures to work

--- change_figure_url.sh
+++ change_figure_url.sh
@@ -2,6 +2,6 @@
 
 for f in docs/labs/R/*.md
   do
-    sed -i 's@https://github.com/senanu/b40/blob/master/b40figure/@https://senanu.github.com/b40/@'
+    sed -i 's@https://github.com/senanu/b40/blob/master/b40figure/@https://senanu.github.com/b40/@' ${f}
   done
   
